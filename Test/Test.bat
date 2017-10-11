3aa5732dbb0d8c6ba374ceea0fa056f3410a9a25
Katie Cookson <katie.cookson@replgroup.com>
2017-10-11 14:59:35 +0100

    made results file yyyymmddhhmmss

--- Test/Test.bat
+++ Test/Test.bat
@@ -1,38 +1,33 @@
 @echo off
 title Run Tests
 
-pause
-
-echo Launch dir: %~dp0%
-echo Current dir: %cd%
-
 if not exist Results mkdir Results
-
-rem make date locale independent
-rem mkdir %date:/=%
 
 
 for /f "skip=1" %%d in ('wmic os get localdatetime') do if not defined mydate set mydate=%%d
 
 
-cd Results
-set foldername=%mydate:~0,8%
-echo foldername: %foldername%
-rem md %mydate:~0,8%
-if not exist %foldername% mkdir %foldername%
-rem md %foldername%
-echo ----- results folder created ----
-cd ..
-echo Current dir: %cd%
+set todaydate=%mydate:~0,8%
+set hour=%time:~0,2%
+set minute=%time:~3,2%
+set second=%time:~6,2%
+set filename=%todaydate%%hour%%minute%%second%
 
-set filename=%time:~0,2%-%time:~3,2%-%time:~6,2%
-echo filename (time) %filename%
+echo todaydate: %todaydate%
+echo hour %hour%
+echo minute %minute%
+echo second %second%
+echo filename %filename%
+
+rem set filename=%foldername%%time:~0,2%%time:~3,2%%time:~6,2%
+rem echo filename (time) %filename%
 
 pause
 @echo about to run tests
 @echo Results/%foldername%/%filename%.xml
 pause
 echo current dir: %cd%
-..\packages\NUnit.ConsoleRunner.3.7.0\tools\nunit3-console /result:Results/%foldername%/%filename%.xml  ../BJSS/bin/debug/BJSS.dll
+rem ..\packages\NUnit.ConsoleRunner.3.7.0\tools\nunit3-console /result:Results/%foldername%/%filename%.xml  ../BJSS/bin/debug/BJSS.dll
+..\packages\NUnit.ConsoleRunner.3.7.0\tools\nunit3-console /result:Results/%filename%.xml  ../BJSS/bin/debug/BJSS.dll
 @echo finished running tests
 pause
